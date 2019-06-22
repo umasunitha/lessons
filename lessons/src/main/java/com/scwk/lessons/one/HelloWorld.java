334863be6038514ee1d7ac18756ed2914f1e1b56
Soham Chakravarti <soham.chakravarti@wolterskluwer.com>
2019-06-22 00:36:22 -0400

    Hello World

---
+++ lessons/src/main/java/com/scwk/lessons/one/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.scwk.lessons.one;
+
+/**
+ * @author Soham.Chakravarti
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+}
